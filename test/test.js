d9f1d8791691834f54549f9f4fe3559a552eeccb
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-12-27 20:33:14 -0800

    Add basic tests

--- test/test.js
+++ test/test.js
@@ -4,3 +4,21 @@ var chai = require('chai');
 var expect = chai.expect;
 // include the file to unit test
 var Shakle = require('../lib/shakle.js');
+
+
+describe('Shakle', function() {
+
+ describe('Existance and Instantiation', function() {
+ 
+  it('should exist', function () {
+    expect(Shakle).to.exist;
+  });
+
+  it('the function should return hte string "hello"', function () {
+    var promise = Shakle;
+    expect(promise()).to.equal('hello');
+  });
+
+ });
+
+});
