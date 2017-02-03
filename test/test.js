d083f798e40cf6954b841aeaeb81390591aee0bf
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-02-03 15:00:21 -0800

    Add place holder for referencing denode

--- test/test.js
+++ test/test.js
@@ -8,10 +8,18 @@ var Shakle = require('../lib/shakle.js');
 
 describe('Shakle', function() {
 
- describe('Existance and Instantiation', function() {
+ describe('The Shakle export', function() {
  
   it('should exist', function () {
     expect(Shakle).to.exist;
+  });
+
+  it('should return instantiate an Ojbject with a test property', function() {
+    expect(new Shakle().test).to.equal('whatever');
+  });
+
+  it('should have a denode property that instantiates its own class', function() {
+    expect(Shakle.denode().test).to.equal('whatever');
   });
 
   // it('the function should return the string "hello"', function () {
