b3fdcae8754ef241c76c77d14c2b0b7b578bf8fd
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-02-04 11:49:00 -0800

    Fleshed out the structure of the tests

--- test/test.js
+++ test/test.js
@@ -14,22 +14,101 @@ describe('Shakle', function () {
       expect(Shakle).to.exist;
     });
 
-    it('should have a promisify method', function () {
-      expect(Shakle.promisify).to.exist;
+    it('should have a "showCallStack" property', function() {
+          expect(Shakle).to.have.property('showCallStack');
     });
 
-    it('should have a "showCallStack" property', function() {
-      expect(Shakle.showCallStack).to.exist;
+    it('should have a "showCallStack" property innitially set to false', function() {
+          expect(Shakle.showCallStack).to.be.false;
+    });
+
+    it('should have a "promisify" method', function () {
+      expect(Shakle).to.have.property('promisify')
+    });
+
+    it('should have a "all" method', function () {
+      expect(Shakle).to.have.property('all')
     });
 
   });
 
   describe ('instantiation', function () {
+    var shakle = new Shakle();
 
     it('should have a test property', function () {
-      expect(new Shakle().test).to.equal('whatever');
+      expect(shakle).to.have.property('state');
     });
 
+  });
+
+  describe('resolution', function () {
+
+    // it('should be called eventually', function (done) {
+    //   setTimeout(function () {
+    //     expect(true).to.be.true
+    //     done()
+    //   }, 500)
+    // })
+
+    var shakledFn1 = function (input) {
+
+      // inside a timeout
+      //does something with the input
+
+      // new Shakle(function (resolve, reject) {
+
+      //   it('should change its state to "resolved"', function () { // THE IT STATEMENT WILL HAVE TO CONTAIN THE TIMEOUT SO YOU CAN USE THE DONE() TRICK
+      //     expect(false).to.be.true
+      //   })
+      // });
+
+    }
+
+    var shakledFn2 = function (input) {
+
+      // inside a timeout
+      //does something with the input
+
+      // new Shakle(function (resolve, reject) {
+
+      //   it('should change its state to "resolved"', function () { // THE CHECKING OF THE VARIBLES/ METHODS IN THE PROMISE WILL HAVE TO BE DONE NOT HERE
+      //     expect(false).to.be.true                                // WILL HAVE TO DO IT SIMILARLY TO BELOW, CHECKING ONE THING AT A TIME BY WRAPPING THE DUPLICATED CHAIN IN INDIVIDUAL IT STATEMENTS DEPENDING ON WHATS BEING TESTED
+      //   })                                                        // PROBABLY BY JUST CALLING IT IN IAN IT STATEMENT AND PROBING THE PROMISE THAT GETS RETURNED 
+      // });
+
+    }
+
+    // THIS WILL HAVE TO BE DONE IN CHUNKS, AN "IT" STATEMENT WRAPPING EACK GROWING CHAIN SO YOU CAN USE THE DONE() TRICK EACH TIME YOU ADD SOMETHING TO THE TEST
+    
+    // shakledFn1(input)
+    //   .then(function (data) {
+        
+    //     it('should pass the correct data to the first .then statement', function () {
+    //       expect(false).to.be.true
+    //     }) 
+
+    //     return shakledFn2(data)
+    //   })
+
+    //   .then(function (data2) {
+        
+    //     it('should pass the correct data to the second .then statement', function () {
+    //       expect(false).to.be.true
+    //     }) 
+    //   })
+
+    //   .catch(function (error) {
+    //     it('should be silent when no errors are present', function () {
+    //       expect(false).to.be.true
+    //     }) 
+    //   })
+
+  })
+
+  describe('rejection', function () {
+    // do the same as resolution but pass in the wrong data
+
+    //then set the showCallStack to true and see test for call stack logging
   })
 
   describe('method:', function () {
@@ -37,8 +116,16 @@ describe('Shakle', function () {
     describe('promisify', function () {
 
       it('should use an instantiate of its own class', function() {
-        expect(Shakle.promisify().test).to.equal('whatever');
+        expect(Shakle.promisify()).to.have.property('state');
       });
+
+    });
+
+    describe('all', function () {
+
+      it('should resolve an array', function () {
+        expect(false).to.be.true;
+      })
 
     })
 
@@ -46,9 +133,16 @@ describe('Shakle', function () {
 
 });
 
+// make sure the promises' variables change as expected during the lifecycle of the promise
+// check that the functions behave respond correctly in isolation, then together
+// NEED SOME SPIES TO SEE IF THE FUNCTIONS ARE BEHAVING CORRECTLY
+// need to check at each phase of the promise lifecycle, probably need to use the done() of stubs or something
+
+
 // range class
 // lrucache
 // asyncmap
+
 
 // SPECS: 
 //
